ba5228e15120c89da728741e18736a7af12c9f67
Firman Ardiyansyah <firman.ardiyansyah@formulatrix.com>
2023-02-08 16:10:11 +0700

    Add DailiRecom; Add LeftWorkHour;

--- WorkingHour/WorkingHourClassLibrary/WorkingHour.cs
+++ WorkingHour/WorkingHourClassLibrary/WorkingHour.cs
@@ -1,78 +1,138 @@
-﻿namespace WorkingHourClassLibrary
-{
-  
-  class FMLXTime
-  {
-    private int _Hour;
-    private int _Minute;
-    private int _Second;
+﻿using Microsoft.Win32;
 
-    public int Hour  
+namespace WorkingHourClassLibrary
+{
+
+  struct FMLXTime
+  {
+    int _Hour = 0;
+    int _Minute = 0;
+    int _Second = 0;
+
+    public int Hour
     {
       get { return _Hour; }
-      set { _Hour = value; }
+      private set { _Hour = value; }
     }
     public int Minute
     {
       get { return _Minute; }
-      set 
+      private set
       {
-        _Minute = value;
-        while( _Minute > 60)
+        if( value > 0 )
         {
-          _Hour++;
-          _Minute -= 60;
+          _Minute = value;
+          while( _Minute >= 60 )
+          {
+            _Hour++;
+            _Minute -= 60;
+          }
         }
+        else if( value < 0 )
+        {
+          _Minute = value;
+          while( _Minute < 0 )
+          {
+            _Hour--;
+            _Minute += 60;
+          }
+        }
+
       }
     }
-
     public int Second
     {
       get { return _Second; }
-      set
+      private set
       {
-        _Second = value;
-        while( _Second > 60 )
+        if( value > 0 )
         {
-          _Minute++;
-          _Second -= 60;
+          _Second = value;
+          while( _Second >= 60 )
+          {
+            _Minute++;
+            _Second -= 60;
+          }
+        }
+        else if( value < 0 )
+        {
+          _Second = value;
+          while( _Second < 0 )
+          {
+            _Minute--;
+            _Second += 60;
+          }
         }
       }
     }
-
-    public FMLXTime(int hour, int minute, int second)
+    public string Value
     {
-      Hour= hour; Minute = minute; Second=second;
+      get
+      {
+        return $"{Hour} : {Minute} : {Second}";
+      }
     }
-    public FMLXTime( int hour, int minute )
+    public FMLXTime( int hour, int minute, int second )
     {
-      Hour = hour; Minute = minute; Second = 0;
+      Hour = hour; Minute = minute; Second = second;
     }
-    public FMLXTime( int hour)
+    public static FMLXTime operator +( FMLXTime a, FMLXTime b )
     {
-      Hour = hour; Minute = 0; Second = 0;
+      return new FMLXTime( a.Hour + b.Hour, a.Minute + b.Minute, a.Second + b.Second );
+    }
+    public static FMLXTime operator -( FMLXTime a, FMLXTime b )
+    {
+      return new FMLXTime( a.Hour - b.Hour, a.Minute - b.Minute, a.Second - b.Second );
+    }
+    public static FMLXTime operator /( FMLXTime a, int b )
+    {
+      return new FMLXTime( a.Hour / b, a.Minute / b, a.Second / b );
     }
   }
 
 
+
+
   public class FMLXWorkingHour
   {
-    //FMLXTime ExpectedWorkingHour = new FMLXTime( 45 );
-    //FMLXTime SuggestedWorkingHour = new FMLXTime( 9 );
-    //FMLXTime BreakTime = new FMLXTime( 0,30 );
 
-    List<FMLXTime> ListOfWeeklyWorkingHour = new List<FMLXTime>();
+    static FMLXTime LeftWorkingHour = new FMLXTime( 45, 0, 0 );
+    static FMLXTime BreakTime = new FMLXTime( 0, 30, 0 );
+    static FMLXTime SuggestionWorkingHour = new FMLXTime( 9, 0, 0 );
+    static int WorkingDay = 5;
 
-    public void Register(int hour, int minute, int second)
+    public void Register( int hour, int minute, int second )
     {
+      var temp = new FMLXTime( hour, minute, second );
+      LeftWorkingHour = LeftWorkingHour - temp;
+      WorkingDay--;
+    }
+    public void GetDailyRecommendation( int hour, int minute, int second )
+    {
+      var arrive = new FMLXTime( hour, minute, second );
+      var recom = LeftWorkingHour / WorkingDay;
+      recom += arrive + BreakTime;
+      var suggest = arrive + SuggestionWorkingHour + BreakTime;
 
+      Console.WriteLine( "DAILY RECOMMENDATION" );
+      Console.WriteLine( $"AVG     : {recom.Value}" );
+      Console.WriteLine( $"SUGGEST : {suggest.Value}" );
+
+    }
+    public void GetLeftWorkingHour()
+    {
+      Console.WriteLine( $"Left {LeftWorkingHour.Value} for {WorkingDay} days" );
     }
 
     public void Test()
     {
-      FMLXTime a = new FMLXTime( 25,50 );
+      Register( 09, 37, 23 );
+      Register( 10, 1, 24 );
 
-      Console.Write( a.Hour );Console.Write(" "); Console.Write( a.Minute );
+
+      GetDailyRecommendation( 7, 36, 48 );
+      GetLeftWorkingHour();
+
     }
 
 
